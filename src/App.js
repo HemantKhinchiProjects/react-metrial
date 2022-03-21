212d0775112d8c45f65ce41dc9a34312eb24a5ac
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-03-21 14:54:49 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Fragment } from 'react';
 import './style.css';
 
 import Header from './Components/Layout/Header';
+import MealSummary from './Components/Meals/MealsSummary';
 export default function App() {
   return (
     <Fragment>
       <Header />
+      <MealSummary />
     </Fragment>
   );
 }
