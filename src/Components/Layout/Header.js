592f57b6bc84caf0db6f0c1e20c5a33a8feab474
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-03-21 13:56:08 +0530

    2 files added

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,15 @@
+import React,{Fragment} from 'react';
+import Header.module.css;
+const Header = () => {
+  return(
+    <Fragment>
+      <header>
+        <h1>Foos App</h1>
+      </header>
+      <div>
+        <img src={}/>
+      </div>
+    </Fragment>
+  )
+};
+export default Header;
